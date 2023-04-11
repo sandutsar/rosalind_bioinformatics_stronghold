5737ab25c67a439c23482adb372da0516bcc559e
Roman Sandutsa <sandutsar@gmail.com>
2023-04-11 23:56:13 +0300

    Fixed constants and removed functions

--- constants.py
+++ constants.py
@@ -1,18 +1,6 @@
 DNA_ALPHABET = 'ACGT'
-RNA_ALPHABET = 'ACGU'
+RNA_ALPHABET = DNA_ALPHABET.replace('T', 'U')
 
 
-COMPLEMENT = {
-    'A': 'T',
-    'C': 'G',
-    'G': 'C',
-    'T': 'A'
-}
-
-
-def is_DNA(nt):
-    return True if nt in DNA_ALPHABET else False
-
-
-def is_RNA(nt):
-    return True if nt in RNA_ALPHABET else False
+COMPLEMENT = {key:value for key, value in \
+              zip(DNA_ALPHABET, DNA_ALPHABET[::-1])}
