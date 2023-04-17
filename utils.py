b99017b68e2761dcfd576971707225e8f806e192
Roman Sandutsa <sandutsar@gmail.com>
2023-04-17 15:53:34 +0300

    Added DNA/RNA complementary tables

--- utils.py
+++ utils.py
@@ -1,9 +1,19 @@
 from constants import DNA_ALPHABET, RNA_ALPHABET
 
-def is_DNA(dna):
-    return all([nt in DNA_ALPHABET for nt in dna])
+def is_DNA(s):
+    return all([nt in DNA_ALPHABET for nt in s])
 
 
-def is_RNA(rna):
-    return all([nt in RNA_ALPHABET for nt in rna])
+def is_RNA(s):
+    return all([nt in RNA_ALPHABET for nt in s])
 
+
+def complement_table(s):
+    if is_DNA(s):
+        return {key:value for key, value in \
+                zip(DNA_ALPHABET, DNA_ALPHABET[::-1])}
+    elif is_RNA(s):
+        {key:value for key, value in \
+                zip(RNA_ALPHABET, RNA_ALPHABET[::-1])}
+    else:
+        raise TypeError(f'Error: Your string must be either DNA or RNA!') 
