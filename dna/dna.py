b53dffaa177e8ef3cb010ecb7298575415d1ea6e
Roman Sandutsa <sandutsar@gmail.com>
2023-04-17 16:46:56 +0300

    Some tasks updates

--- dna/dna.py
+++ dna/dna.py
@@ -1,19 +1,20 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), \
                              os.pardir))
-from constants import DNA_ALPHABET, is_DNA
+from constants import DNA_ALPHABET
 from exceptions import IterableLengthError, StringIsNotDNAError
+from utils import is_DNA
 
 
 def dna(s, path=None, save=False):
     # assert isinstance(s, str), f'Error: type(s) = {type(s).__name__} must be str!'
-    # assert all(list(map(_is_DNA, s))), f'Error: Your string is not a valid DNA string! \
+    # assert is_DNA(s), f'Error: Your string is not a valid DNA string! \
     #     It must be composed using {DNA_ALPHABET} alphabet!'
     # assert len(s) <= 1e3, f'Error: len(s) = {len(s)} must be <= 1000!'
 
     if not isinstance(s, str):
         raise TypeError(f'Error: type(s) = {type(s).__name__} must be str!')
-    if not all(list(map(is_DNA, s))):
+    if not is_DNA(s):
         raise StringIsNotDNAError()
     if len(s) > 1e3:
         raise IterableLengthError(iterable=s, sign='<=', value=int(1e3))
@@ -32,9 +33,12 @@ def dna(s, path=None, save=False):
     return result
 
 if __name__ == '__main__':
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), \
-                           'rosalind_dna_1_dataset.txt'), 'r') as file:
-        s = file.readline()[:-1]
+    if len(sys.argv) == 2:
+        s = sys.argv[1]
+    else:
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), \
+                            'rosalind_dna_1_dataset.txt'), 'r') as file:
+            s = file.readline()[:-1]
 
     print(s)
 
--- revc/revc.py
+++ revc/revc.py
@@ -1,24 +1,25 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), \
                              os.pardir))
-from constants import COMPLEMENT, is_DNA
+# from constants import DNA_ALPHABET
 from exceptions import IterableLengthError, StringIsNotDNAError
+from utils import complement_table, is_DNA
 
 
 def revc(s, path=None, save=False):
     # assert isinstance(s, str), f'Error: type(s) = {type(s).__name__} must be str!'
-    # assert all(list(map(_is_DNA, s))), f'Error: Your string is not a valid DNA string! \
+    # assert is_DNA(s), f'Error: Your string is not a valid DNA string! \
     #     It must be composed using {DNA_ALPHABET} alphabet!'
     # assert len(s) <= 1e3, f'Error: len(s) = {len(s)} must be <= 1000!'
 
     if not isinstance(s, str):
         raise TypeError(f'Error: type(s) = {type(s).__name__} must be str!')
-    if not all(list(map(is_DNA, s))):
+    if not is_DNA(s):
         raise StringIsNotDNAError()
     if len(s) > 1e3:
         raise IterableLengthError(iterable=s, sign='<=', value=int(1e3))
     
-    result = [COMPLEMENT[nt] for nt in s]
+    result = [complement_table(s)[nt] for nt in s]
     result.reverse()
     result = ''.join(result)
     
@@ -35,9 +36,12 @@ def revc(s, path=None, save=False):
 
 
 if __name__ == '__main__':
-    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), \
-                           'rosalind_revc_1_dataset.txt'), 'r') as file:
-        s = file.readline()[:-1]
+    if len(sys.argv) == 2:
+        s = sys.argv[1]
+    else:
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), \
+                            'rosalind_revc_1_dataset.txt'), 'r') as file:
+            s = file.readline()[:-1]
 
     print(s)
 
